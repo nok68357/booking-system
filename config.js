45fa6d9650f31da5f5f5ba8caa51642669f5b92b
nok68357 <75023141+nok68357@users.noreply.github.com>
2025-03-08 13:34:12 +0000

    3

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    aprUrl: "https://script.google.com/macros/s/AKfycbxVUinvWzsp178oiD7ldBJFlc3J-Bke1377PsJ9uuBIbrKgI-TlxuO0xtzmqSV3af0b/exec"
+    aprUrl: "https://script.google.com/macros/s/AKfycbzRIbO515VmvJWcZnctYvtOPVdc9_dDp-f8Zs3Q7dGKWIN8-h0yUrbY1a4xDz-ViAif/exec"
 }
